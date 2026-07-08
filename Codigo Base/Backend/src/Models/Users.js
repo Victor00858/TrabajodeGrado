76ae274ae386762fe30801bd96d8771301e2d47d
Victor Artunduaga <victorartunduaga00858@gmail.com>
2026-07-08 11:27:31 -0500

    Actualizacion

--- Codigo Base/Backend/src/Models/Users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const UserSchema = new Schema({
-    usuario : String,
-    password: String
-});
-
-module.exports = mongoose.model('Users', UserSchema);
--- Codigo Base/Backend/src/index.js
+++
@@ -1,7 +0,0 @@
-
-const app = require('./app');
-
-//Starting the server
-app.listen(app.get('port'), () => {
-    console.log('servidor a su servicio en el puerto', app.get('port'))
-});
